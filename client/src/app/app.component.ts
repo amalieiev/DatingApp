0adba64eb5b111d9093f23fac7388e69059baabf
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-26 00:19:14 +0300

    generated angular app

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'client';
+  public users = this.http.get('https://localhost:5001/api/users');
+
+  public constructor(private http: HttpClient) {}
 }
