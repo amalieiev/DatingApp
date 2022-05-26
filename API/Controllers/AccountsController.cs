c91f20a66bae0705b751f4be63aac72870e2ced4
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-26 15:50:01 +0300

    added register endpoint

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -1,4 +1,7 @@
+using System.Security.Cryptography;
+using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,9 +18,29 @@ public class AccountController : BaseApiController
     }
 
 
-    [HttpGet("test")]
-    public int Get()
+    [HttpPost("register")]
+    public async Task<ActionResult<AppUser>> Register(RegisterDto data)
     {
-        return 110;
+        using var hmac = new HMACSHA512();
+        var users = await _context.Users.ToListAsync();
+        var user = users.Find(item => item.UserName == data.Username);
+
+        if (user is not null)
+        {
+            return null;
+        }
+
+        user = new AppUser
+        {
+            UserName = data.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        _context.Users.Add(user);
+
+        await _context.SaveChangesAsync();
+
+        return user;
     }
 }
