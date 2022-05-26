f73591f9f9c5734e1c6bcaedf105bbb50e80ecb0
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-26 12:53:16 +0300

    updated user entity

---
+++ API/Data/Migrations/20220526092813_AddedPassword.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class AddedPassword : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "UserPasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "UserPasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserPasswordSalt",
+                table: "Users");
+        }
+    }
+}
