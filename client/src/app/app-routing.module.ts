5a39f8f300a7d99cfceced3bb5302546aa037c48
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-27 12:50:07 +0300

    added routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,15 +3,26 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
     {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'users',
+    },
+    {
         path: 'users',
         loadChildren: () =>
             import('./users/users.module').then((m) => m.UsersModule),
     },
     {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'users',
+        path: 'list',
+        loadChildren: () =>
+            import('./list/list.module').then((m) => m.ListModule),
     },
+    {
+        path: 'messages',
+        loadChildren: () =>
+            import('./messages/messages.module').then((m) => m.MessagesModule),
+    },
+    { path: 'matches', loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule) },
 ];
 
 @NgModule({
