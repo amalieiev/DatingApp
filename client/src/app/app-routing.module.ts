5902edba9134980fd7899cce29a3c7ead967eee0
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-27 11:23:01 +0300

    used prettier for reformating all files

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'users',
-    loadChildren: () =>
-      import('./users/users.module').then((m) => m.UsersModule),
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'users',
-  },
+    {
+        path: 'users',
+        loadChildren: () =>
+            import('./users/users.module').then((m) => m.UsersModule),
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'users',
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- client/src/app/users/users-routing.module.ts
+++ client/src/app/users/users-routing.module.ts
@@ -5,7 +5,7 @@ import { UsersComponent } from './users.component';
 const routes: Routes = [{ path: '', component: UsersComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class UsersRoutingModule {}
