d0438d83c68f67d5f1c182732477f6915c905f84
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-27 15:58:35 +0300

    added profile dropdown

--- client/src/app/shared/ui/nav/nav.module.ts
+++ client/src/app/shared/ui/nav/nav.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
     declarations: [NavComponent],
-    imports: [CommonModule, ReactiveFormsModule],
+    imports: [CommonModule, ReactiveFormsModule, BsDropdownModule.forRoot()],
     exports: [NavComponent],
 })
 export class NavModule {}
