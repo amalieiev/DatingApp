74779aa1571e338048e47d34624fc8106a60f361
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-30 15:59:46 +0300

    added router links

--- client/src/app/shared/ui/nav/nav.module.ts
+++ client/src/app/shared/ui/nav/nav.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { AppRoutingModule } from '../../../app-routing.module';
 
 @NgModule({
     declarations: [NavComponent],
-    imports: [CommonModule, ReactiveFormsModule, BsDropdownModule.forRoot()],
+    imports: [CommonModule, AppRoutingModule, ReactiveFormsModule, BsDropdownModule.forRoot()],
     exports: [NavComponent],
 })
 export class NavModule {}
