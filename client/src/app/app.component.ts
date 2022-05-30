68d8ee168362c9d4e071c06aaf1ae90269c89e24
artem.malieiev <artem.malieiev@exclaimer.com>
2022-05-30 21:33:55 +0300

    added navigation after login/logout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from './shared/data-access/account.service';
 import { Observable } from 'rxjs';
 import { IUser } from './shared/interfaces/user.interface';
 import { IUserAccount } from './shared/interfaces/user-account.interface';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -12,17 +13,20 @@ import { IUserAccount } from './shared/interfaces/user-account.interface';
 export class AppComponent implements OnInit {
     public user: Observable<IUserAccount | null> = this.account.user;
 
-    public constructor(private account: AccountService) {}
+    public constructor(private account: AccountService, private router: Router) {}
 
     public ngOnInit(): void {
         this.account.isAuthorized();
     }
 
     public onLogin(user: IUser) {
-        this.account.login(user).subscribe();
+        this.account.login(user).subscribe(() => {
+            this.router.navigateByUrl('/matches');
+        });
     }
 
     public onLogout() {
         this.account.logout();
+        this.router.navigateByUrl('/home');
     }
 }
